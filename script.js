56e23eeba4c189f3ba9fff6644447cd2923ee2c0
[Emmanuel <[emmamusculos@gmail.com]>
2023-05-28 21:57:12 -0600

    Cuarto commit

--- script.js
+++ script.js
@@ -16,10 +16,24 @@ let cuentas = [
     },
 ]
 
+function sesion(){
+    let usuario = document.getElementById("user").value
+    let contra = document.getElementById("password").value
+
+    if(usuario == "" || contra == ""){
+        alert("Por favor ingresa los datos.")
+    } else {
+        alert("Hay algo")
+    }
+}
+
 function inicio(id) {
+    // Encontrar el id del usuario con el username
     if(cuentas[id].pass == prompt("¿Cuál es la contraseña?")){
+        // comparar si tiene la misma contraseña en la locacion
         console.log("Nombre: " + cuentas[id].nombre)
         console.log("Saldo actual: " + cuentas[id].saldo)
+        // Redireccionar a la página principal
     } else{
         console.log("Contraseña incorrecta")
     }    
